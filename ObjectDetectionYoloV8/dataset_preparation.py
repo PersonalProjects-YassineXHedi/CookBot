cc31b1b9192d33dd7527bfeb2b9af1b9b4b122cd
Yassine SAHLI <ysahl091@uottawa.ca>
2025-03-27 20:16:12 -0400

    added a method that creates a new dataset following the old dataset partition ratio

--- ObjectDetectionYoloV8/dataset_preparation.py
+++ ObjectDetectionYoloV8/dataset_preparation.py
@@ -2,6 +2,7 @@ from ultralytics.utils import yaml_load
 import os 
 import random
 import shutil
+import math 
 
 DATA_PATH = '/home/yassine/GitRepo/Data'
 YOLO_DATA_PATH = "/home/yassine/GitRepo/Data/yolo_dataset_v1"
@@ -35,16 +36,16 @@ def get_classes_images_and_labels(data_path = YOLO_DATA_PATH):
 
     names = data['names']
     classes_images_and_labels = {}
-    for name in names:
-        image_path_list = []
-        diff_dir = [ 'val', 'test','train']
-        for dir_name in diff_dir:
-            label_dir = data[dir_name].replace('/images', '/labels')
-            image_dir = data[dir_name]
+    diff_dir = [ 'val', 'test','train']
+    for dir_name in diff_dir:
+        classes_images_and_labels_specific_dir = {}
+        label_dir = data[dir_name].replace('/images', '/labels')
+        image_dir = data[dir_name]
 
-            label_files = sorted(os.listdir(label_dir))
-            img_files = sorted(os.listdir(image_dir))
-
+        label_files = sorted(os.listdir(label_dir))
+        img_files = sorted(os.listdir(image_dir))
+        for name in names:
+            class_image_path_list = []
             for label_file, img_file in zip(label_files, img_files):
                 label_path = os.path.join(label_dir, label_file)
                 image_path = os.path.join(image_dir, img_file)
@@ -52,38 +53,50 @@ def get_classes_images_and_labels(data_path = YOLO_DATA_PATH):
                 for line in open_file:
                     class_id = int(line.strip().split()[0])
                     if(class_id == names.index(name)):
-                        image_path_list.append((image_path,label_path))
+                        class_image_path_list.append((image_path,label_path))
                         break
-        classes_images_and_labels[name] = image_path_list
+            classes_images_and_labels_specific_dir[name] = class_image_path_list
+        classes_images_and_labels[dir_name] = classes_images_and_labels_specific_dir
     return classes_images_and_labels
 
+
+
+
 def get_class_images_labels(classes_images_and_labels, class_name, percent = 100):
-    images_and_labels_one_class = []
-    images_and_labels = classes_images_and_labels[class_name]
     if(percent > 100 or percent <=0):
-        return
-    num = (percent*len(images_and_labels))/100
-    i=0
-    while(i<num):
-        value = random.randint(0,len(images_and_labels))
-        images_and_labels_one_class.append(images_and_labels[value])
-        i+=1
+            return
+    images_and_labels_one_class = {}
+    diff_dir = [ 'val', 'test','train']
+    for dir_name in diff_dir:
+        images_and_labels_one_class_specific_dir = []
+        images_and_labels_specific_dir = classes_images_and_labels[dir_name][class_name]
+        
+        num = math.ceil((percent * len(images_and_labels_specific_dir)) / 100)
+        i=0
+        while(i<num):
+            value = random.randint(0,len(images_and_labels_specific_dir))
+            images_and_labels_one_class_specific_dir.append(images_and_labels_specific_dir[value])
+            i+=1
+        images_and_labels_one_class[dir_name] = images_and_labels_one_class_specific_dir
     return images_and_labels_one_class
 
 
-def create_dir_with_specific_images(folder_name, image_and_label_path_list, folder_path = DATA_PATH):
+def try_create_dir_and_push_data_for_specific_class(folder_name, image_and_label_path_dict, folder_path = DATA_PATH):
     #create dir 
     path = folder_path + '/' + folder_name
-    os.makedirs(path, exist_ok=True)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
     #create images and labels dir
     images_dir_path = path + '/images/'
     labels_dir_path = path + '/labels/'
-    os.makedirs(images_dir_path, exist_ok=True)
-    os.makedirs(labels_dir_path, exist_ok=True)
+    if not os.path.exists(images_dir_path):
+        os.makedirs(images_dir_path)
+    if not os.path.exists(labels_dir_path):
+        os.makedirs(labels_dir_path)
 
     #push the images and labels path to the corresponding dir
-    for img_path, label_path in image_and_label_path_list:
+    for img_path, label_path in image_and_label_path_dict[folder_name]:
         shutil.copy(img_path, images_dir_path)
         shutil.copy(label_path, labels_dir_path)
 
@@ -97,13 +110,14 @@ def get_first_n_classe(n, data_path = YOLO_DATA_PATH):
 
 def create_dataset(classes_names, percent, new_dataset_folder_path, dataset_folder_name, full_dataset_path = YOLO_DATA_PATH):
     classes_images_and_labels = get_classes_images_and_labels(full_dataset_path)
-    dataset_folder_full_path = new_dataset_folder_path + '/' + dataset_folder_name
-    os.makedirs(dataset_folder_full_path, exist_ok=False)
-    for class_name in classes_names:
-        images_and_labels_one_class = get_class_images_labels(classes_images_and_labels, class_name,percent)
-        diff_dir = [ 'val', 'test','train']
-        for dir_name in diff_dir:
-            create_dir_with_specific_images(dir_name, images_and_labels_one_class, dataset_folder_full_path)
+    new_dataset_folder_full_path = new_dataset_folder_path + '/' + dataset_folder_name
+    os.makedirs(new_dataset_folder_full_path, exist_ok=False)
+    
+    diff_dir = [ 'val', 'test','train']
+    for dir_name in diff_dir:
+        for class_name in classes_names:
+            images_and_labels_one_class = get_class_images_labels(classes_images_and_labels, class_name,percent)
+            try_create_dir_and_push_data_for_specific_class(dir_name, images_and_labels_one_class, new_dataset_folder_full_path)
 
-create_dataset(['ground beef', 'onion'], 1, DATA_PATH,'test_v1_1_percent')
+
 
