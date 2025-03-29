b9118ffa4c5e9d83eba23f07c54cb1d65396c74b
Hedi Bouassida <hbouassida.esct@gmail.com>
2025-03-29 15:08:22 -0400

    saving commit

--- ObjectDetectionYoloV8/model_creation.py
+++ ObjectDetectionYoloV8/model_creation.py
@@ -1,16 +1,26 @@
 from ultralytics import YOLO
 import os
 from adaptative_path_function import adapt_path
+import matplotlib.pyplot as plt
 
+DATA_PATH = adapt_path("/home/hboua/GitRepo/Data/CandyDataset")
 
-
-
-path = adapt_path("/home/hboua/GitRepo/Data/CandyDataset/data.yaml")
-print(path)
-
+#Load a model
 model = YOLO("yolov8s.pt")
 
-results = model.train(data=path, epochs=100, imgsz=640)
+# train model
+results = model.train(data=os.path.join(DATA_PATH,  ), epochs=100, imgsz=640)
+
 
 prediction = model.predict("/home/hboua/GitRepo/Data/CandyDataset/test/images/all-type-of-cup_000069_jpg.rf.bdfe0ca8227bd023cc956d59af11fbe2.jpg")
 print(prediction[0])
+
+
+# show image with bounding boxes
+image = prediction[0].plot()
+plt.imshow(image)
+plt.title(class_names[label.numpy()]) # add title to image by indexing on class_names list
+plt.axis(False);
+
+print("about to show image")
+
